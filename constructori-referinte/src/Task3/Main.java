67c903b171a2b35eaf6f2dfd1f0f26c4c3869ea5
Adelin Stanca <adelin_stanca@yahoo.com>
2020-10-22 13:38:01 +0300

    labul nr. 2

---
+++ constructori-referinte/src/Task3/Main.java
@@ -0,0 +1,10 @@
+package Task3;
+
+public class Main {
+    public static void main(String[] args) {
+        float[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Polygon poligon = new Polygon(numbers);
+
+        poligon.printPolygon();
+    }
+}
